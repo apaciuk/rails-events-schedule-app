fea222b4b59f7b857aba94b7e9a52086a827a832
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-03 18:21:07 +0100

    Devise

--- db/schema.rb
+++ db/schema.rb
@@ -88,6 +88,11 @@ ActiveRecord::Schema.define(version: 2021_07_03_161047) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.datetime "announcements_last_read_at"
